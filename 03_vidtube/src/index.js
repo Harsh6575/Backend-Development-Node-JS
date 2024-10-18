bac9d2ff2f0aaa9c34c609614485aee13e6716e4
“Harsh6575” <harsh.190410107146@gmail.com>
2024-10-18 11:31:05 +0530

    03 vidtube connect db professionally

--- 03_vidtube/src/index.js
+++ 03_vidtube/src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from backend and using nodemon");
+import dotenv from "dotenv";
+// import 'dotenv/config'
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+// set up port number
+const PORT = process.env.PORT ?? 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}/`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1); // Exit with an error code if the connection fails.
+  });
